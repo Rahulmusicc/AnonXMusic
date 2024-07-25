39aa4e55dee9f62afc30b9644d0860c6868bfd54
unitereapermusic <158771773+unitereapermusic@users.noreply.github.com>
2024-07-25 10:13:25 +0530

    Update config.py
    Signed-off-by: unitereapermusic <158771773+unitereapermusic@users.noreply.github.com>

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-
+DEV_ID = int(5871038439)
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/unitereapermusic/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
