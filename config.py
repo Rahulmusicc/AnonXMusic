615bb147c5fe620fc725df8151979047bb33edbd
unitereapermusic <158771773+unitereapermusic@users.noreply.github.com>
2024-07-25 11:46:41 +0530

    Update config.py
    Signed-off-by: unitereapermusic <158771773+unitereapermusic@users.noreply.github.com>

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/Unite-reaper-02-03"
+    "START_IMG_URL", "https://telegra.ph/file/7ff36e0b486d8fd0bb7f6.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
